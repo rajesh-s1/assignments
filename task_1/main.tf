b58df5129aa1b1e80bc1252a4d4973afaa9c4ad9
rajgithub181 <rajesh.ace99@gmail.com>
2023-06-26 12:50:04 +0530

    some changes, vars, data

--- task_1/main.tf
+++ task_1/main.tf
@@ -25,30 +25,47 @@ resource "azurerm_virtual_network" "kvnet" {
 }
 
 
-#CREATE 3 SUBNETS
-#SUBNET-1
-resource "azurerm_subnet" "kfes" {
-  name                 = "k_fes"
+#CREATE 3 SUBNETS WITH FOR_EACH - frontend, backend, db
+resource "azurerm_subnet" "ksubs" {
+  for_each = local.tiers
+
+  name                 = "${each.key}-subnet"
   resource_group_name  = var.rg_name
   virtual_network_name = azurerm_virtual_network.kvnet.name
-  address_prefixes     = ["10.1.1.0/24"]
+  address_prefixes     = ["${each.value}"]
 }
 
-#SUBNET-2
-resource "azurerm_subnet" "kbes" {
-  name                 = "k_bes"
-  resource_group_name  = var.rg_name
-  virtual_network_name = azurerm_virtual_network.kvnet.name
-  address_prefixes     = ["10.1.2.0/24"]
+
+resource "azurerm_network_interface" "knics" {
+  for_each = local.tiers
+  name                = "${each.key}-nic"
+  location            = var.location
+  resource_group_name = var.rg_name
+
+  ip_configuration {
+    name                          = "${each.key}-nic-config"
+    subnet_id                     = azurerm_subnet.ksubs[each.key].id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
-#SUBNET-3
-resource "azurerm_subnet" "kdbs" {
-  name                 = "k_dbs"
-  resource_group_name  = var.rg_name
-  virtual_network_name = azurerm_virtual_network.kvnet.name
-  address_prefixes     = ["10.1.3.0/24"]
+
+output "network_interface_private_ip" {
+  description = "virtual network interface private ip"
+  value = [for int in azurerm_network_interface.knics: int.private_ip_address ]
 }
+
+data "azurerm_network_interface" "fe_private_ip" {
+  depends_on = [ azurerm_network_interface.knics ]
+  name = "fe-nic"
+  resource_group_name = var.rg_name
+}
+
+output "fe_private_ip" {
+  description = "fe virtual network interface private ip"
+  value = "${data.azurerm_network_interface.fe_private_ip.private_ip_address}"
+}
+
 
 #SUBNET-4 FOR APPLICATION GATEWAY
 resource "azurerm_subnet" "kags" {
@@ -59,50 +76,6 @@ resource "azurerm_subnet" "kags" {
 }
 
 
-
-#CREATE 3 NETWORK INTERFACE
-#NIC-1
-resource "azurerm_network_interface" "knic1" {
-  name                = "k-nic1"
-  location            = var.location
-  resource_group_name = var.rg_name
-
-  ip_configuration {
-    name                          = "frontend-tier-NIC"
-    subnet_id                     = azurerm_subnet.kfes.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-#NIC-2
-resource "azurerm_network_interface" "knic2" {
-  name                = "k-nic2"
-  location            = var.location
-  resource_group_name = var.rg_name
-
-  ip_configuration {
-    name                          = "backend-tier-NIC"
-    subnet_id                     = azurerm_subnet.kbes.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-#NIC-3
-resource "azurerm_network_interface" "knic3" {
-  name                = "k-nic3"
-  location            = var.location
-  resource_group_name = var.rg_name
-
-  ip_configuration {
-    name                          = "db-tier-NIC"
-    subnet_id                     = azurerm_subnet.kdbs.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-
-
-
 #CREATE A RANDOM PASSWORD FOR 3 VMS
 resource "random_password" "vmpass" {
   length = 10
@@ -110,17 +83,17 @@ resource "random_password" "vmpass" {
   numeric = true
 }
 
-#CREATE 3 VMS
-#VM-1
 
-resource "azurerm_virtual_machine" "kvm1" {
-  name                  = "k-vm1"
+
+resource "azurerm_virtual_machine" "kvms" {
+  for_each = local.tiers
+  name                  = "${each.key}-vm"
   location              = var.location
   resource_group_name   = var.rg_name
-  network_interface_ids = [azurerm_network_interface.knic1.id]
+  network_interface_ids = [azurerm_network_interface.knics[each.key].id]
   vm_size               = "Standard_B1s"
   storage_os_disk {
-    name              = "vmdisk1"
+    name              = "${each.key}disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
@@ -132,84 +105,18 @@ resource "azurerm_virtual_machine" "kvm1" {
     version   = "latest"
   }
   os_profile {
-    computer_name  = "k-vm1"
-    admin_username = "k-vm1-user"
+    computer_name  = "${each.key}-vm"
+    admin_username = "${each.key}-vm-user"
     admin_password = "${random_password.vmpass.result}"
   }
+#  admin_ssh_key {
+#  username = "admin-user"
+#  public_key = file(<path-to-.pub>)
+#  }
   os_profile_linux_config {
     disable_password_authentication = false
   }
 }
-
-
-#VM-2
-
-resource "azurerm_virtual_machine" "kvm2" {
-  name                  = "k-vm2"
-  location              = var.location
-  resource_group_name   = var.rg_name
-  network_interface_ids = [azurerm_network_interface.knic2.id]
-  vm_size               = "Standard_B1s"
-  storage_os_disk {
-    name              = "vmdisk2"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-  os_profile {
-    computer_name  = "k-vm2"
-    admin_username = "k-vm2-user"
-    admin_password = "${random_password.vmpass.result}"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-}
-
-
-#VM-3
-
-resource "azurerm_virtual_machine" "kvm3" {
-  name                  = "k-vm3"
-  location              = var.location
-  resource_group_name   = var.rg_name
-  network_interface_ids = [azurerm_network_interface.knic3.id]
-  vm_size               = "Standard_B1s"
-  storage_os_disk {
-    name              = "vmdisk3"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-  os_profile {
-    computer_name  = "k-vm3"
-    admin_username = "k-vm3-user"
-    admin_password = "${random_password.vmpass.result}"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-}
-
-
-output "fe-vm-private-ip" {
-  description = "Private ip of fe vm"
-  value = "${azurerm_network_interface.knic1.private_ip_address}"
-  #value = "${azurerm_network_interface.knic1.*.private_ip_address}"
-}
-
 
 
 #PUBLIC IP FOR APPLICATION GATEWAY
@@ -220,16 +127,13 @@ resource "azurerm_public_ip" "pip" {
   allocation_method   = "Dynamic"
 }
 
-
-
-
 #APPLICATION GATEWAY 
 
 resource "azurerm_application_gateway" "kag" {
   name                = "k-ag"
   resource_group_name = var.rg_name
   location            = var.location
-
+  depends_on = [azurerm_network_interface.knics]
   sku {
     name     = "Standard_Small"
     tier     = "Standard"
@@ -253,7 +157,7 @@ resource "azurerm_application_gateway" "kag" {
 
   backend_address_pool {
     name = "be_add_pool"
-    ip_addresses = ["${azurerm_network_interface.knic1.private_ip_address}"]
+    ip_addresses = ["${data.azurerm_network_interface.fe_private_ip.private_ip_address}"]
   }
 
   backend_http_settings {
--- task_1/variables.tf
+++ task_1/variables.tf
@@ -12,3 +12,11 @@ variable "location" {
   type = string
   default = "South India"
 }
+
+locals {
+  tiers = {
+    "fe" = "10.1.1.0/24"
+    "be" = "10.1.2.0/24"
+    "db" = "10.1.3.0/24"
+  }
+}
